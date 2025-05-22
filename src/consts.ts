8f2d77cb47a99cf2d1f5242ca52bdf35b4ea36e5
Yusuf <yusufturhag@outlook.com>
2025-05-22 21:07:09 +0300

    Refactor site structure and content: rename project to 'portfolio', update site title and description, enhance 'about' page with personal information and experience, and improve layout responsiveness in 'Header.astro' and 'index.astro'.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Yusuf Turhan";
+export const SITE_DESCRIPTION = "Yazılım Geliştirici | Web Teknolojileri Uzmanı | Full Stack Developer";
